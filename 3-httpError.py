7d911c2d04e72317724b55c07a06f7af0bad5e22
Wellington Dos Santos gama <36666479+wellingtongama1998@users.noreply.github.com>
2022-06-02 21:26:33 -0300

    Add files via upload

---
+++ 3-httpError.py
@@ -0,0 +1,24 @@
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from urllib.error import URLError
+
+try:
+    html= urlopen('http://www.google.com')
+except HTTPError as e:
+    print(e)    
+    #Devolve null,executa um break ou algum outro 'Plano B'
+except URLError as e:
+    print("Servidor ou url não existe")
+
+else:
+    #Opcional
+    #O programa continua.
+
+#HTTP ERROR
+#Se um código de erro HTTP for devolvido.o programa agora exibirá o erro
+#E não executará o resto do programa que está na intrução else.
+
+#URL ERROR
+#Se o servidor não for encontrado,isso quer dizer que não foi possível acessar
+#nenhum servidor.url está incorreta ou servidor fora do ar.
+#URLOPEN lançará um URlERROR.
