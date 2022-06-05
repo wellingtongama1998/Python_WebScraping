591120798e6b2985692b263d64d110fabf921019
Wellington Dos Santos gama <36666479+wellingtongama1998@users.noreply.github.com>
2022-06-05 19:37:02 -0300

    Update 3-httpError.py

--- 3-httpError.py
+++ 3-httpError.py
@@ -4,6 +4,7 @@ from urllib.error import URLError
 
 try:
     html= urlopen('http://www.google.com')
+    print("Okay")
 except HTTPError as e:
     print(e)    
     #Devolve null,executa um break ou algum outro 'Plano B'
@@ -22,3 +23,7 @@ else:
 #Se o servidor não for encontrado,isso quer dizer que não foi possível acessar
 #nenhum servidor.url está incorreta ou servidor fora do ar.
 #URLOPEN lançará um URlERROR.
+
+#ATTRIBUTE ERROR
+#Tentar acessar uma tag que não existe devolverá objeto none,e resultará no lançamento de um AttributeError.
+
