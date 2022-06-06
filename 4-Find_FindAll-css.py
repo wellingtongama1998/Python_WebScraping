a2d9ee829a0951d95b6888c044a1037a4231ceb3
Wellington Dos Santos gama <36666479+wellingtongama1998@users.noreply.github.com>
2022-06-06 15:34:07 -0300

    Update 4-Find_FindAll-css.py

--- 4-Find_FindAll-css.py
+++ 4-Find_FindAll-css.py
@@ -40,3 +40,36 @@ for name in namelist:
 #ex: .find_all('span',{'class':{'green','red'}})
 
 #TEXT
+#É peculiar por fazer a correspondência com base no conteúdo textual das tags.
+#Se quiser encontrar o número de vezes que "the prince" está cercado por tags na página.
+#Ex: nameList = bs.find_all(text='the prince')
+#    print(len(namelist))
+
+#LIMIT
+#É usado somente no método FIND_ALL com um limite igual a 1.
+#Esse parâmetro pode ser definido se você estiver apenas em obter os primeiros x itens da página.
+
+#KEYWORD
+#Permite selecionar tags que contenham um atributo ou um conjunto de atributos específicos.
+#EX: title= bs.find_all(id='title',class_='text')
+#Devolve a primeira tag com a palavra "text" no atributo class_ e "title" no atributo id.
+#Uma linha como essa talvez não seja particulamente útil,e deveria ser equivalente a:
+# title= bs.find(id='title')
+
+#AGUMENTOS NOMEADOS E "CLASS"
+#O argumento keyword pode ser útil em algumas situações,etretanto,ele é tecnicamente redundante como um recurso
+#do BeautifulSoup.
+#Tenha em mente que tudo que puder ser feito com keyword também poderá ser feito com técnicas discutidas
+#mais adiante com "expressões regulares" e "Expressões lambda"
+
+#Você pode deparar com problemas ao usar KEYWORD, se estiver procurando atributo CLASS,pois CLASS é uma palavra
+#reservada protegida em Python,ou seja,ela não pode ser usada como uma variável ou como nome de argumento
+#Soulução: como alterniva ,CLASS pode ser usada entre aspas:
+           #bs.find_all('', {'class':'green'})
+#Se tivermos uma lista de tags longa,poderemos acabar com muita informação indesejada.
+#O argumento KEYWORD permite acrescentar um filtro 'e'.
+
+#
+         
+
+
