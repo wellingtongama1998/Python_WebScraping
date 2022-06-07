92b2dd7baf0014f1237fae59c145c6aec63b016a
Wellington Dos Santos gama <36666479+wellingtongama1998@users.noreply.github.com>
2022-06-07 15:07:39 -0300

    Update 5-Arvores.py

--- 5-Arvores.py
+++ 5-Arvores.py
@@ -1,4 +1,18 @@
-#Capitulo 1 vimos como navegar em arvore em uma única direção:
-# bs.tag.subTag.outraSubTag
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
 
-#Vamos agora ver como navegar para cima,para os lados e na diagonal em árvores HTML.
+html = urlopen('http://www.pythonscraping.com/pages/page3.html')
+bs = BeautifulSoup(html.read(), 'html.parser')
+for child in bs.find('table', {'id': 'giftList'}).descendants:
+    print(child
+
+
+# Capitulo 1 vimos como navegar em arvore em uma única direção:
+#Ex: bs.tag.subTag.outraSubTag
+
+# Vamos agora ver como navegar para cima,para os lados e na diagonal em árvores HTML.
+    
+#.CHILDREN
+#Se quiser encontrar somente os descendentes que sejam filhos
+#.DESCENDANTS
+#Se fôssemos escrevê-lo no lugar de children(),aproximadamente duas dúzias de 
