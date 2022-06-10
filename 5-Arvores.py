ca7984bcba6847871dd733d7adcd4c628302ee19
Wellington Dos Santos gama <36666479+wellingtongama1998@users.noreply.github.com>
2022-06-10 14:18:02 -0300

    Update 5-Arvores.py

--- 5-Arvores.py
+++ 5-Arvores.py
@@ -16,3 +16,30 @@ for child in bs.find('table', {'id': 'giftList'}).descendants:
 #Se quiser encontrar somente os descendentes que sejam filhos
 #.DESCENDANTS
 #Se fôssemos escrevê-lo no lugar de children(),aproximadamente duas dúzias de tags.
+
+          
+#IRMÃOS
+#next_siblings().
+          #Esse código tem como saída todas as linhas de produtos da tabela,exceto a primeira linha 
+          #com os títulos.
+          #(next) próximos irmãos.
+          #Solução: for child in bs.find('table', {'id': 'giftList'}).tr:
+
+#previous_siblings().
+          #Muitas vezes pode ser útil se houver uma tag facilmente selecionável no final de uma lista de tags
+          #irmãs que você queira obter.
+ 
+          #next_subling() e previous_sibling().
+           #Devolvem uma única tag,em vez de devolver uma lista delas.
+          
+          
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+html = urlopen('http://www.pythonscraping.com/pages/page3.html')
+bs = BeautifulSoup(html.read(), 'html.parser')
+#for child in bs.find('table', {'id': 'giftList'}).children:
+for child in bs.find('table', {'id': 'giftList'}).tr.next_siblings:
+    print(child)
+
+          
